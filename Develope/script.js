9e56885828879706d9c5015dfb1dfde77eafe6e7
Sahil Patel <sahildepatel@gmail.com>
2022-03-19 22:37:01 -0400

    Adding the if statements for the alert promt

--- Develope/script.js
+++ Develope/script.js
@@ -1,17 +1,106 @@
 // Assignment Code
 var enter;
-var numbers;
-var specialCharacters;
-var uppercase;
-var lowercase;
+var confirmNumber;
+var confirmSpecials;
+var confirmUpper;
+var confirmLower;
+var choices;
 
-function generatePassword
+
+var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var toUpper = function(x){
+  return x.toUpperCase();
+};
+var upperLetters = lowerLetters.map(toUpper);
+
+var space = [];
+
+function generatePassword();
 var generateBtn = document.querySelector("#generate");
 function generateBtn(){
   window.alert("Yess or no?")
 }
 // Write password to the #password input
 function writePassword() {
+  //User input for how many characters
+  userInput = parseInt(prompt("How many characters would you like your password length? Enter between 8 and 128"));
+
+  if(!userInput){
+    alert("You must enter a number.");
+  }
+  else if (enter < 8 || enter > 128){
+    alert("You must enter a number between 8 and 100.")
+  }
+  else {
+    confirmNumber = confirm("Do you want numbers?");
+    confirmSpecials = confirm("Do you want special characters");
+    confirmUpper = confirm("Do you want uppercase letters?");
+    confirmLower = confirm("Do you want lowercase letters?");
+  };
+  // If all inputs are negative
+  if (!confirmLower && !confirmNumber && !confirmSpecials && !confirmUpper){
+    choices = alert("You must choose at least one of the criteria.")
+  }
+
+  else if (confirmLower && confirmNumber && confirmSpecials && confirmUpper){
+    choices = upperLetters.concat(numbers, specialCharacters, lowerLetters);
+  }
+//For 3 positive options 
+
+ else if (confirmNumber && confirmSpecials && confirmUpper){
+   choices = numbers.concat(specialCharacters, upperLetters);
+ }
+ else if (confirmNumber && confirmSpecials && confirmLower){
+   choices = numbers.concat(specialCharacters, lowerLetters);
+ }
+ else if (confirmNumber && confirmUpper && confirmLower){
+  choices = numbers.concat(specialCharacters, lowerLetters);
+}
+else if (confirmUpper && confirmSpecials && confirmLower){
+  choices = numbers.concat(specialCharacters, lowerLetters);
+}
+
+//For 2 Positive options 
+else if (confirmNumber && confirmSpecials){
+  choices = numbers.concat(specialCharacters);
+}
+else if (confirmNumber && confirmUpper){
+  choices = numbers.concat(upperLetters);
+
+}else if (confirmNumber && confirmLower){
+  choices = numbers.concat(lowerLetters);
+
+}else if (confirmUpper && confirmSpecials){
+  choices = upperLetters.concat(specialCharacters);
+
+}else if (confirmUpper && confirmLower){
+  choices = upperLetters.concat(lowerLetters);
+
+}else if (confirmLower && confirmSpecials){
+  choices = lowerLetters.concat(specialCharacters);
+}
+
+//For 1 positive
+else if (confirmNumber){
+  choices = numbers;
+}
+else if (confirmLower){
+  choices = lowerLetters;
+
+}else if (confirmUpper){
+  choices = upperLetters;
+
+}else if (confirmSpecials){
+  choices = specialCharacters;
+};
+var password = [];
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
